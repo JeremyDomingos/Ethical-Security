14dc9d0d2bf3068cb8b5e42e7d4533ec357c24d9
bob the kali <codejeremydomingos@gmail.com>
2026-03-30 10:34:17 +0200

    Add requester.py for handling HTTP requests and responses

---
+++ KeyShot/main.c
@@ -0,0 +1,10 @@
+/*
+*   NOTE: FOR EDUCATIONAL PURPOSES ONLY, DO NOT USE WITHOUT AUTHORIZATION/CONSENT ON 
+*   A NETWORK/DEVICE YOUR RUN THIS APPLICATION ON OR FOR CRIMINAL ACTIVITY
+*
+*
+*
+*   This is a research project looking into how keyloggers work by 
+*   attempting to build one for both windows and linux based devices.
+*   
+*/
